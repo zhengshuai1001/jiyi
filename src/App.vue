cfb3df06e438b39274ec3954df5d2e59ea784360
Zheng Shuai <864737869@qq.com>
2018-06-11 18:24:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld  @click="aa"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    aa() {
+      alert("aaa");
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/css/global.scss";
+</style>
