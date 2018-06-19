8807ca362455663f44061628a229d9ad2af880d2
Zheng Shuai <864737869@qq.com>
2018-06-19 17:58:44 +0800

    下班前提交

--- src/components/RunMap.vue
+++ src/components/RunMap.vue
@@ -1,7 +1,14 @@
 <template>
 	<div class="run-map-page">
-	<div class="demo">
+	<div class="canvas-box">
 		<canvas id="myCanvas" width="850" height="650"></canvas>
+		<div 
+			class="lifting-arm-img" 
+			:style="{ 
+				'left': liftingArmImg[0].left + 'px',
+				'top': liftingArmImg[0].top + 'px',
+				'transform': 'rotateX(45deg) rotateZ('+liftingArmImg[0].rotation+'deg)',
+			}"></div>
 	</div>
 	</div>
 </template>
@@ -11,9 +18,25 @@ export default {
 	data() {
 		return {
 			ctx: null,
+			liftingArmImg: [
+				{
+					left: 0,
+					top: 0,
+					rotation: 0,
+				},
+			]
 		}
 	},
 	mounted() {
+		// this.liftingArmImg[0] = {
+		// 	left: 100,
+		// 	top: 200,
+		// 	rotation: 90,
+		// }
+		this.liftingArmImg[0].left = 100;
+		this.liftingArmImg[0].top = 200;
+		this.liftingArmImg[0].rotation = 90;
+
 		this.ctx = document.getElementById("myCanvas").getContext("2d");
 		this.drawRoundedRect(this.ctx, 20, 140, 107, 360, 40, 10, true, false, 2, "#595959", "#959595");
 		this.drawRoundedRect(this.ctx, 143, 140, 107, 360, 10, 10, true, false, 2, "#595959", "#959595");
@@ -26,6 +49,20 @@ export default {
 
 		//绘画煤仓
 		this.drawCoalBunker();
+
+		//绘画煤堆
+		//测试，先画一个煤堆试试
+		this.drawCoalHeapOne(this.ctx, require("../assets/CoalHeap/1-101.png"), 23, 170, 100, 183.4 );
+		this.drawCoalHeapOne(this.ctx, require("../assets/CoalHeap/1-103.png"), 146, 170, 100, 95.6 );
+
+		this.drawCoalHeapOne(this.ctx, require("../assets/CoalHeap/2-102.png"), 313, 170, 100, 89.2 );
+		this.drawCoalHeapOne(this.ctx, require("../assets/CoalHeap/2-104.png"), 436, 170, 100, 82 );
+
+		this.drawCoalHeapOne(this.ctx, require("../assets/CoalHeap/3-201.png"), 603, 170, 100, 206.4 );
+		this.drawCoalHeapOne(this.ctx, require("../assets/CoalHeap/3-203.png"), 723, 170, 100, 81.4 );
+		
+		//画一个斗轮机
+		this.drawBucketWheelOne(this.ctx, 130, 201, 45, 0);
 
 	},
 	methods: {
@@ -61,18 +98,42 @@ export default {
 		drawCoalBunker() {
 
 			let startX = 754.5;
-			let starty = 44.5;
+			let starty = 34.5;
 
 			for (let i = 0; i < 4; i++) {
 				for (let j = 0; j < 6; j++) {
-					this.drawCoalBunkerOne(this.ctx, startX, starty);
+					let fontText = "A";
+					switch (j) {
+						case 0:
+								fontText = "A";
+							break;
+						case 1:
+								fontText = "B";
+							break;
+						case 2:
+								fontText = "C";
+							break;
+						case 3:
+								fontText = "D";
+							break;
+						case 4:
+								fontText = "E";
+							break;
+						case 5:
+								fontText = "F";
+							break;
+						default:
+							break;
+					}
+					this.drawCoalBunkerOne(this.ctx, startX, starty, [], fontText, "#78a8e8", i);
 					startX -= 24;
 				}
 				startX -= 42;
 			}
 
 		},
-		drawCoalBunkerOne(ctx, x, y, mcArray = []) {
+		//画一个煤仓
+		drawCoalBunkerOne(ctx, x, y, mcArray = [], fontText, fontColor, outerIndex) {
 
 			ctx.save();
 			ctx.translate(x, y);
@@ -115,9 +176,62 @@ export default {
 			ctx.lineTo(10, 41);
 			ctx.lineTo(1, 20);
 			ctx.closePath();
+
+			//渲染煤仓下的文字,煤仓编号
+			ctx.font = '16px Arial';
+			ctx.fillStyle = fontColor;
+			ctx.textAlign = 'center';
+			ctx.fillText(fontText, 10, 60);
 			ctx.stroke();
+
+			//渲染煤仓下的机组编号
+			if (fontText == "D") {
+				ctx.beginPath();
+				ctx.font = '14px Arial';
+				ctx.fillStyle = "#8a8a8a";
+				ctx.textAlign = 'start';
+				ctx.fillText(`#${outerIndex + 1}机组`, 0, 76);
+				ctx.closePath();
+			}
 			ctx.restore();
-		}
+		},
+		//画一个煤堆
+		drawCoalHeapOne(ctx, src, x, y, w, h ) {
+			let imageObj = new Image();
+			imageObj.src = src;
+			imageObj.onload = () => {
+				// ctx.save();
+				ctx.drawImage(imageObj, x, y, w, h);
+				// ctx.restore();
+			};
+		},
+		//画一个斗轮机
+		drawBucketWheelOne(ctx, x, end, rotation, status){
+			let y = 140 + 360 - (end * 1.2);
+			let imageObj = new Image();
+			imageObj.src = require("../assets/fuselage.png"); //斗轮机的机身部分
+			imageObj.onload = () => {
+				// ctx.save();
+				ctx.drawImage(imageObj, x, y, 10, 42.7);
+				// ctx.restore();
+
+				//画斗轮机的吊臂
+				this.drawLiftingArm(x, y, rotation, status);
+			};
+		},
+		//画斗轮机的吊臂
+		drawLiftingArm(x, y, rotation, status) {
+			// this.liftingArmImg[0] = {
+			// 	left: x,
+			// 	top: y,
+			// 	rotation,
+			// }
+			// this.liftingArmImg[0].left = x;
+			// this.liftingArmImg[0].top = y;
+			// this.liftingArmImg[0].rotation = rotation;
+
+			console.log(this.liftingArmImg[0]);
+		},
 	}
 }
 </script>
